21a8f8942205004ff56056a9f5463daae00ba340
Hanna <hanna.shalamitskaya@gmail.com>
2018-09-25 13:24:44 +0300

    Homework7: Hanna Shalamitskaya

--- examples.py
+++ examples.py
@@ -3,9 +3,6 @@ from money.models import AmountCurrency
 
 if __name__ == "__main__":
     # create instances
-    # looks like service currencylayer.com provide a rate for BYR
-    # in 10 000 (before denomination)
-    # 1 USD is 19600
     x = AmountCurrency(19600, "BYN")
     y = AmountCurrency(11)
     z = AmountCurrency(12.34, "EUR")
@@ -17,5 +14,7 @@ if __name__ == "__main__":
            AmountCurrency(11),
            AmountCurrency(12.01, "JPY")]
     s = sum(lst)
+
     # result in “BYN”
     print(s)
+
